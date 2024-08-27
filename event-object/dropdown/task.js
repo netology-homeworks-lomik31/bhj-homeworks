7f8991d85e5ef929627dc5dd0d906000c738bb1c
lomik31 <lomik31@codead.dev>
2024-08-27 19:01:14 +0300

    1.3.1 fix

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -5,7 +5,7 @@ document.getElementById("click__dropdown").addEventListener("click", function() 
 document.querySelectorAll(".dropdown__item").forEach(function(item) {
     item.addEventListener("click", function(e) {
         e.preventDefault();
-        document.querySelectorAll("dropdown__value").forEach(e =>
+        document.querySelectorAll(".dropdown__value").forEach(e =>
             e.textContent = item.textContent
         )
         document.getElementsByClassName("dropdown__list")[0].classList.remove("dropdown__list_active");
